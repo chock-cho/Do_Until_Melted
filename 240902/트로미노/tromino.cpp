b5f8408309be4c141be3da5ab923ec8f3e44bc69
Sujung Shin <113707388+chock-cho@users.noreply.github.com>
2024-09-02 09:38:05 +0900

    Title: 트로미노, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240902/트로미노/tromino.cpp
+++ 240902/트로미노/tromino.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 const int dx[] = {-1, 0, 1, 0};
 const int dy[] = {0, 1, 0, -1};
-int n, m;
 int grid[201][201];
+int n, m;
 int ans = -987654321;
+
+bool isValid(int x, int y){
+    return (x>=0)&&(y>=0)&&(x<n)&&(y<m);
+}
 
 int main() {
     cin >> n >> m;
@@ -17,30 +20,31 @@ int main() {
 
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
+
             int tmp = 0;
-            if(i+2 < n){
-                tmp = grid[i][j] + grid[i+1][j] + grid[i+2][j];
+
+            if(i+2 < n) {
+                tmp = grid[i][j]+grid[i+1][j]+grid[i+2][j];
                 ans = max(tmp, ans);
             }
 
             if(j+2 < m){
-                tmp = grid[i][j] + grid[i][j+1] + grid[i][j+2];
+                tmp = grid[i][j]+grid[i][j+1]+grid[i][j+2];
                 ans = max(tmp, ans);
             }
 
             for(int k = 0; k < 4; k++){
-                int nx_1 = i + dx[k];
-                int ny_1 = j + dy[k];
+                int nx1= i + dx[k];
+                int ny1= j + dy[k];
 
-                int nx_2 = i + dx[(k+1)%4];
-                int ny_2 = j + dy[(k+1)%4];
+                int nx2= i+ dx[(k+1)%4];
+                int ny2= j+ dy[(k+1)%4];
 
-                if(nx_1 < 0 || nx_2 < 0 || nx_1 >= n || nx_2 >= m 
-                || ny_1 < 0 || ny_2 < 0 || ny_1 >= n || ny_2 >= m) continue;
-
-                tmp = grid[i][j] + grid[nx_1][ny_1] + grid[nx_2][ny_2];
-                ans = max(tmp, ans);
+                if(!isValid(nx1, ny1) || !isValid(nx2, ny2)) continue;
+                tmp = grid[i][j]+grid[nx1][ny1]+grid[nx2][ny2];
+                ans = max(tmp, ans);  
             }
+
         }
     }
     cout << ans;
