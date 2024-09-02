315d52fc2156d227d3b71356940fe5f88fd6e49e
Sujung Shin <113707388+chock-cho@users.noreply.github.com>
2024-09-02 12:10:49 +0900

    Title: 행복한 수열의 개수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240902/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 240902/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -2,27 +2,35 @@
 using namespace std;
 int n, m;
 int grid[101][101];
-int ans=0;
+int ans1, ans2=0;
 int sol(){
     int cnt1; // 열 비교
     for(int r = 0; r < n; r++){
-       cnt1 =1;
+       cnt1 =1; bool flag = false; // 해당 열에서 이미 겹치는 수가 나왔나
         for(int c = 1; c < n; c++){
-            if(grid[r][c-1]==grid[r][c]) cnt1+=1;
+            if(grid[r][c-1]==grid[r][c]){ 
+                cnt1+=1; flag=true;
+                if(cnt1 >= m) {
+                    ans1++; break;
+                }
+            }
+            else if(flag) cnt1= 1;
         }
-        //cout << cnt1 << " \n";
-        if(cnt1 >= m) ans++;
     }
     int cnt2; // 행 비교
     for(int c = 0; c < n; c++){
-        cnt2= 1;
+        cnt2= 1; bool flag = false;
         for(int r =1; r <n; r++){
-            if(grid[r-1][c]==grid[r][c]) cnt2+=1;
+            if(grid[r-1][c]==grid[r][c]) {
+                cnt2+=1; flag = true;
+                if(cnt2 >= m) {
+                    ans2++; break;
+                }
+            }
+            else if(flag) cnt2=1;
         }
-        //cout << cnt2 << "\n";
-        if(cnt2 >= m) ans++;
     }
-    return ans;
+    return ans1+ans2;
 }
 
 int main() {
@@ -31,6 +39,9 @@ int main() {
         for(int j = 0; j < n; j++){
             cin >> grid[i][j];
         }
+    }
+    if(m==1) {
+        cout << n+n; return 0;
     }
     cout << sol();
     return 0;
