3abe3927ad35bd403b79d6c2b92b2335c69ec124
Sujung Shin <113707388+chock-cho@users.noreply.github.com>
2024-09-06 18:51:05 +0900

    Title: 1차원 바람, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240906/1차원 바람/The-1D-wind-blows.cpp
+++ 240906/1차원 바람/The-1D-wind-blows.cpp
@@ -3,7 +3,6 @@ using namespace std;
 int N, M, Q;
 int grid[104][104];
 string priorDir;
-int cnt = 0;
 void leftPush(int r){
     priorDir = "L";
     int tmp = grid[r][M]; //cout << tmp << "\n";
@@ -22,13 +21,6 @@ void rightPush(int r){
     grid[r][M] = tmp;
 }
 
-bool upChk(int r){
-    for(int i = 1; i <= M; i++){
-        if(grid[r-1][i]==grid[r][i]) return true;
-    }
-    return false;
-}
-
 bool blowChk(int K, int r){
     for(int i = 1; i <= M; i++){
         if(grid[K][i]==grid[r][i]) return true;
@@ -37,28 +29,40 @@ bool blowChk(int K, int r){
 }
 
 void upBlow(int r){
+    if(r <= 1) return;
+    //cout << r << "열, 위로 바람 전파\n";
+    if(!blowChk(r-1, r)) return;
     if(r >= 2 && blowChk(r-1, r)){
-        if(priorDir == "L") rightPush(r-1);
-        else leftPush(r-1);
+        (priorDir == "L")? rightPush(r-1) : leftPush(r-1);
         upBlow(r-1);
+        return;
     }
     return;
 }
 
-void downBlow(int r, string originDir){  
-    if(cnt==0) priorDir = originDir;
-    if(r <= M && blowChk(r+1, r)){
-        //cout << blowChk << "\n";
-        if(priorDir == "L") rightPush(r+1);
-        else leftPush(r+1);
-        downBlow(r+1, priorDir);
+void downBlow(int r, string originDir, int cnt){  
+    if(r >= N) return;
+     //cout << r << "열, 아래로 바람 전파\n";
+    if(cnt==0) priorDir = originDir; 
+    if(!blowChk(r+1, r)) return;
+    if(r <= N-1 && blowChk(r+1, r)){
+        //cout << priorDir << "\n";
+        (priorDir == "L")? rightPush(r+1) : leftPush(r+1);
+       downBlow(r+1, priorDir, cnt);
+       return;
     }
     return;
 }
 
 void windBlow(int r, string originDir){
-    upBlow(r); downBlow(r, originDir);
+    int r1 = r;
+    int r2 = r;
+    //cout << "====New Turn====\n";
+    upBlow(r1);
+    //cout << "--------------\n";
+     downBlow(r2, originDir, 0); 
 }
+
 int main() {
     cin >> N >> M >> Q;
     for(int i = 1; i <= N; i++){
@@ -68,17 +72,16 @@ int main() {
     }
     while(Q--){
         priorDir = "";
-        int r; string cmd="";
+        string cmd="";
         string originDir="";
-        cin >> r >> cmd;
-        if(cmd == "L"){
-            leftPush(r);
-        }
-        else if(cmd == "R"){
-            rightPush(r);
-        }
-        originDir = priorDir;
-        windBlow(r, originDir);
+        int row;
+        cin >> row >> cmd;
+        (cmd == "L")? leftPush(row) : rightPush(row);
+        // 5 5 5 2             // 5 5 5 2
+        // 5 4 0 2 // 2 5 4 0 // 2 5 4 0
+        // 1 1 4 3 // 1 1 4 3 // 1 4 3 1
+        originDir = priorDir; //R
+        windBlow(row, originDir); //1, R
     }
     for(int i = 1; i <= N; i++){
         for(int j = 1; j <= M; j++){
