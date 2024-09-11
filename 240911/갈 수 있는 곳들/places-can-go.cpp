6c20f09cf32965cecc7dfbba9100777d51168642
Sujung Shin <113707388+chock-cho@users.noreply.github.com>
2024-09-11 23:52:09 +0900

    Title: 갈 수 있는 곳들, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240911/갈 수 있는 곳들/places-can-go.cpp
+++ 240911/갈 수 있는 곳들/places-can-go.cpp
@@ -8,23 +8,32 @@ int n,k;
 int grid[101][101];
 bool vis[101][101];
 queue<pi> q;
-int cnt = 1;
+int cnt= 0;
 
 bool inGrid(int x, int y){
-    return (x>=0)&&(x<n)&&(y>=0)&&(y<n);
+    return (x>=1)&&(x<=n)&&(y>=1)&&(y<=n);
+}
+
+bool canGo(int x, int y){
+    if(!inGrid(x,y)) return false;
+    if(vis[x][y]) return false;
+    if(grid[x][y]==1) return false;
+    return true;
 }
 
 int main() {
     cin >>n>> k;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
             cin >> grid[i][j];
         }
     }
     int r, c;
     while(k--){
         cin >> r >> c;
-        vis[r][c] = true;
+        if(canGo(r,c)) cnt+=1;
+
+        vis[r][c]= true;
         q.push(make_pair(r,c));
         while(!q.empty()){
             int r = q.front().first;
@@ -34,10 +43,8 @@ int main() {
             for(int i = 0; i < 4; i++){
                 int nr = r +dr[i];
                 int nc = c +dc[i];
-                if(!inGrid(nr,nc)) continue;
-                if(vis[nr][nc]) continue;
-                if(grid[nr][nc]==1) continue;
 
+                if(!canGo(nr,nc)) continue;
                 cnt+=1;
                 vis[nr][nc] =true;
                 q.push(make_pair(nr,nc));
