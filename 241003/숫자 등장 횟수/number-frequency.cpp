d7da707635ce9b7a675653bff1a0d7f116f2cc5c
Sujung Shin <113707388+chock-cho@users.noreply.github.com>
2024-10-03 17:12:35 +0900

    Title: 숫자 등장 횟수, Time: 195ms, Memory: 1MB, Status: Passed - CodeTree

--- 241003/숫자 등장 횟수/number-frequency.cpp
+++ 241003/숫자 등장 횟수/number-frequency.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 #define MAX_VAL 100001
 int n, m;
-int arr[MAX_VAL];
+unordered_map<int,int> ump;
 int main() {
     cin >> n >> m;
     for(int i = 0; i < n; i++){
         int val; cin >> val;
-        arr[val]+=1;
+        ump[val]+=1;
     }
     for(int i = 0; i < m; i++){
         int query; cin >> query;
-        cout << arr[query] << " ";
+        cout << ump[query] << " ";
     }
     return 0;
 }
