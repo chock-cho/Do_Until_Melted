274db6e46ae2a710d45ca1eae2a0a160ddb79ee0
Sujung Shin <113707388+chock-cho@users.noreply.github.com>
2024-10-13 09:44:53 +0900

    Title: 특정 수와 근접한 합, Time: 1079ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 241013/특정 수와 근접한 합/sum-close-to-particular-number.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+int S, N;
+int arr[101]; int sum=0;
+vector<int> v;
+int ans=987654321;
+
+void dfs(int cnt, int prev_idx){
+    if(cnt==2){
+        int cur_sum=sum;
+        cur_sum= sum-arr[v[0]]-arr[v[1]];
+        ans=min(abs(cur_sum-S), ans);
+    }
+    for(int i = prev_idx+1; i <= N; i++){
+        v.push_back(i);
+        dfs(cnt+1,i);
+        v.pop_back();
+    }
+}
+
+int main() {
+    cin >> N >> S;
+    for(int i = 0; i < N; i++){
+        cin >> arr[i]; 
+        sum+=arr[i];
+    }
+    dfs(0,-1);
+    cout <<ans;
+    return 0;
+}
