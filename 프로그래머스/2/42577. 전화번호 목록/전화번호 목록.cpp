82a8cffd4320401ada42d443a72eebd534032d08
Sujung Shin <113707388+chock-cho@users.noreply.github.com>
2024-10-15 08:01:34 +0900

    hashMap solution 방법- 전화번호 목록.cpp

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -4,10 +4,20 @@
 using namespace std;
 
 bool solution(vector<string> phone_book) {
-    bool answer = true;
-    sort(phone_book.begin(), phone_book.end());
-    for(int i = 0; i < phone_book.size()-1; i++){
-        if(phone_book[i+1].find(phone_book[i])==0) return false;
+    // sol1. sort를 이용
+    // bool answer = true;
+    // sort(phone_book.begin(), phone_book.end());
+    // for(int i = 0; i < phone_book.size()-1; i++){
+    //     if(phone_book[i+1].find(phone_book[i])==0) return false;
+    // }
+    // return true;
+
+    //sol2. hashMap(unordered_map STL 이용)
+    for(int i = 0; i < phone_book.size(); i++) {
+        for(int j = 0; j < phone_book[i].size(); j++) {
+            string s= phone_book[i].substr(0,j);
+            if(ump.find(s)!=ump.end()) return false;
+        }
     }
     return true;
 }
