c0f9a22100928cec88e7be039915adcec743650c
Sujung Shin <113707388+chock-cho@users.noreply.github.com>
2024-11-16 05:30:33 +0900

    Title: 호텔 예약, Time: 96ms, Memory: 2MB, Status: Passed - CodeTree

--- 241116/호텔 예약/reserve-hotel.cpp
+++ 241116/호텔 예약/reserve-hotel.cpp
@@ -9,7 +9,7 @@ int main() {
     cin >> n;
     for(int i = 0; i < n; i++){
         int s,e; cin >> s>> e;
-        points.push_back(make_pair(s, +1));
+        points.push_back(make_pair(s-1, +1));
         points.push_back(make_pair(e, -1));
     }
     int curCnt=0;
@@ -17,10 +17,7 @@ int main() {
     for(int i = 0; i < points.size(); i++){
         int x,v; tie(x,v)=points[i]; // (3, +1) (5, -1)
         curCnt+=v;
-        int prev_x = points[i-1].first;
-        if(i>=1 && prev_x==x && v == +1) curCnt+=1;
         // (1, +1) (3, -1) --> (1, +1) (3, -1) (3, +1) (5, -1)
-        //if(prev_x==x && curCnt==0) curCnt+=1;
         roomCnt=max(roomCnt, curCnt);
     }
     cout << roomCnt;
