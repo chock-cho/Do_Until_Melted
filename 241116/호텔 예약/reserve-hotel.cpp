526507387f1a88f80986055dba46f033989a6284
Sujung Shin <113707388+chock-cho@users.noreply.github.com>
2024-11-16 05:24:06 +0900

    Title: 호텔 예약, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241116/호텔 예약/reserve-hotel.cpp
+++ 241116/호텔 예약/reserve-hotel.cpp
@@ -15,8 +15,12 @@ int main() {
     int curCnt=0;
     sort(points.begin(), points.end());
     for(int i = 0; i < points.size(); i++){
-        int x,v; tie(x,v)=points[i]; 
+        int x,v; tie(x,v)=points[i]; // (3, +1) (5, -1)
         curCnt+=v;
+        int prev_x = points[i-1].first;
+        if(i>=1 && prev_x==x && v == +1) curCnt+=1;
+        // (1, +1) (3, -1) --> (1, +1) (3, -1) (3, +1) (5, -1)
+        //if(prev_x==x && curCnt==0) curCnt+=1;
         roomCnt=max(roomCnt, curCnt);
     }
     cout << roomCnt;
